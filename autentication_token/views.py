dd108c6d87a57f91ed6a48ae5d9087e1e8242db3
Samir <spruiz97@gmail.com>
2024-05-26 16:18:47 -0400

    Update views.py
    desarrollo del login y de una funcion profile para probar la autenticacion

--- autentication_token/views.py
+++ autentication_token/views.py
@@ -1,7 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth.models import User
 from .serializers import UserSerializer 
@@ -9,7 +12,16 @@ from .serializers import UserSerializer
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.data['username'])
+    
+    if not user.check_password(request.data['password']):
+        return Response({"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    
+    serializer = UserSerializer(instance=user)
+        
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def register(request):
@@ -28,5 +40,7 @@ def register(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
-def profile(request):
-    return Response({})
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def profile(request):    
+    return Response("You are login with {}".format(request.user.username), status=status.HTTP_200_OK)
